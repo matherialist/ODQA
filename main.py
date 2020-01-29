e8ba766925c2d604bbaca3f8101ec6cc90611ef6
Aleksei <lagrange@phystech.edu>
2020-01-29 15:11:11 +0300

    Heaven and Hell!

--- main.py
+++ main.py
@@ -1,22 +1,16 @@
 from deeppavlov.core.common.file import read_json
 from deeppavlov import configs, train_model
 from deeppavlov.core.commands.infer import build_model
-from deeppavlov.utils.telegram import interact_model_by_telegram
 
 
-model_config = read_json("configs/ru_ranker_tfidf_wiki.json")
-#model_config = read_json(configs.doc_retrieval.en_ranker_tfidf_wiki)
-#model_config["dataset_reader"]["data_path"] = "~/Desktop/ODQA/train_data"
-#model_config["dataset_reader"]["dataset_format"] = "txt"
+if __name__ == '__main__':
+    model_config = read_json("configs/en_ranker_tfidf_wiki.json")
 
-ranker = train_model(model_config)
-ranker(['cerebellum'])
+    ranker = train_model(model_config)
 
-squad = build_model(configs.squad.multi_squad_ru_retr_noans_rubert_infer, download=True)
+    squad = build_model(configs.squad.multi_squad_noans_infer, download=True)
 
-odqa = build_model("/configs/ru_odqa_infer_wiki_rubert_noans.json", download=False)
-answers = odqa(["what is tuberculosis?", "how should I take antibiotics?"])
+    odqa = build_model("configs/en_odqa_infer_wiki.json", download=False)
+    answers = odqa(["what is tuberculosis?"])
 
-print(answers)
-
-#interact_model_by_telegram(model_config=configs.odqa.en_odqa_infer_wiki, token='')
+    print(answers)
